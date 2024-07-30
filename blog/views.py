0da8effc6da8f46e43bd99df3a10538a6e1ae81a
alinasiricom <nasiri.ali.r@gmail.com>
2024-07-30 08:58:02 +0330

    set data in Blog-home

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import *
 
 def home_view(request):
-    return render(request, 'blog/blog-home.html')
+    posts = Post.objects.filter(status=True)
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html', context)
 
 def single_view(request):
     return render(request, 'blog/blog-single.html')
