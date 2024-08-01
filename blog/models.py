be09e531e8bda48b3514541c4af4b14816872932
alinasiricom <nasiri.ali.r@gmail.com>
2024-08-01 09:31:44 +0330

    Blog / add author
    add author field to Blog

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     #
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
     #
