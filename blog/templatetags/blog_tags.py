62108f231b1ff0547a06e47d051a7f7949e86907
alinasiricom <nasiri.ali.r@gmail.com>
2024-08-07 09:58:25 +0330

    Blog / Post Categories

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -9,3 +9,15 @@ def latestposts(n=5):
     posts = Post.objects.filter(status=1, 
         published_date__lte = timezone.now()).order_by('-published_date')[:n]
     return {'posts': posts}
+
+@register.inclusion_tag('blog/blog-posts-categories.html')
+def postscategories():
+    cats = Category.objects.all()
+    posts = Post.objects.filter(status=1, published_date__lte = timezone.now())
+    pcats_dict = {}
+    
+    for c in cats:
+        pcats_dict[c] = posts.filter(category=c).count()
+    pcats_dict = dict(sorted(pcats_dict.items(), key=lambda x: -x[1]))        # sort dict
+    
+    return {'pcats': pcats_dict}
